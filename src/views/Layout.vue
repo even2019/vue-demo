364be65ecd4b658d6c8ac4f57debb4e795ab5067
frank <frank@163.com>
2019-07-30 14:28:05 +0800

    视频中心页面编写

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -1,22 +1,24 @@
 <template>
   <div>
     <div class="header">
-      <div class="logo">logo</div>
-      <div class="nav-bar">
-        <div class="nav-link" v-for="(item, index) in navList" :key="index">
-          <router-link :to="item.Link">{{item.Name}}</router-link>
-          <span class="nav-gap"></span>
+      <div class="h-content clear-fix">
+        <div class="logo">logo</div>
+        <div class="nav-bar">
+          <div class="nav-link" v-for="(item, index) in navList" :key="index">
+            <router-link :to="item.Link">{{item.Name}}</router-link>
+            <span class="nav-gap"></span>
+          </div>
         </div>
-      </div>
-      <div class="login">
-        <span class="content">
-          <img src="../assets/images/phone.png" alt="">
-          <a>下载手机客户端</a>
-          <span class="down-arrow"></span>
-        </span>
-        <span class="content">
-          <a>登录</a>或<a>注册</a>
-        </span>
+        <div class="login">
+          <span class="content">
+            <img src="../assets/images/phone.png" alt="">
+            <a>下载手机客户端</a>
+            <span class="down-arrow"></span>
+          </span>
+          <span class="content">
+            <a>登录</a>或<a>注册</a>
+          </span>
+        </div>
       </div>
     </div>
     <div class="main">
--- src/views/VideoCenter/index.vue
+++ src/views/VideoCenter/index.vue
@@ -1,5 +1,41 @@
 <template>
-  <div class="video-center">视频中心</div>
+  <div class="video-center">
+    <div class="banner">
+        <div class="banner-content">
+          <div class="banner-title">
+            <h1>视频中心</h1>
+          </div>         
+          <div class="vid-search">
+            <div class="search-contain">
+              <div class="search-inp">
+                <input type="text">
+              </div>             
+              <div class="search-btn">
+                <p>搜索</p>
+              </div>
+            </div>
+            <div class="top-search">
+              <div class="search-label">热搜</div>
+              <ul class="search-title-list">
+                <li>2009</li>
+                <li>紫色水离子</li>
+                <li>MISS</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+    </div>
+    <div class="vid-container">
+      <div class="crumbs"></div>
+      <div class="vid-play-content"></div>
+      <div class="vid-label"></div>
+      <div class="vid-list"></div>
+      <div class="advert"></div>
+      <div class="comment-operate"></div>
+      <div class="comment-list"></div>
+    </div>
+    
+  </div>
 </template>
 
 <script>
