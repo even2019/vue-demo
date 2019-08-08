a6869d2e114355a635155769e9f8cf92d6691d91
sandy <2561348952@qq.com>
2019-08-08 14:17:39 +0800

    register页面

--- src/components/global/MapSearch/Index.vue
+++ src/components/global/MapSearch/Index.vue
@@ -4,46 +4,29 @@
     <div class="map-wrap">
       <h1 class="map-title">注册</h1>
       <div class="sear-box">
-        <div class="sear-bar"></div>
+        <div class="sear-bar">
+          <input v-model="title" type="text" class="sear-input" name="title" placeholder="搜索关键字" />
+          <input type="submit" value="搜索" class="sear-btn" />
+        </div>
         <div class="sear-history">
-          <dl>
-            <dt>热搜</dt>
-            <dd>WCA2015</dd>
-            <dd>NEWBEE</dd>
-            <dd>T15国际邀请赛</dd>
-          </dl>
+          <span>热搜</span>
+          <a href="javascript:;">WCA2015</a>
+          <a href="javascript:;">NEWBEE</a>
+          <a href="javascript:;">T15国际邀请赛</a>
         </div>
       </div>
     </div>
-    <!-- <div class="banner-content">
-      <div class="banner-title">
-        <h1>视频中心</h1>
-      </div>
-      <div class="vid-search">
-        <div class="search-contain">
-          <div class="search-inp">
-            <input type="text" />
-          </div>
-          <div class="search-btn">
-            <p>搜索</p>
-          </div>
-        </div>
-        <div class="top-search">
-          <div class="search-label">热搜</div>
-          <ul class="search-title-list">
-            <li>2009</li>
-            <li>紫色水离子</li>
-            <li>MISS</li>
-          </ul>
-        </div>
-      </div>
-    </div>-->
   </div>
 </template>
 
 <script>
 export default {
   name: "MapSearch",
+  data () {
+    return {
+      title: "",
+    }
+  }
 }
 </script>
 
