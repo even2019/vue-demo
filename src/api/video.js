37b2e18db6ee36a6fa392b4bcbc375f3cfc80313
frank <frank@163.com>
2019-08-12 21:51:36 +0800

    新增轮播

--- src/api/video.js
+++ src/api/video.js
@@ -9,99 +9,131 @@ export default [
               {
                 url: 'http://www.axshare.cn/gsc/7ANRJJ/0c/d0/8a/0cd08ad2866f481fa4e58bb5e69ca4b0/images/视频中心/u8358.png?token=b4f9ca86ea1c42b7cd6db6c052ef27dd41aeb1a86c3e332c26ed878b44b55847',
                 name:'DOTA2',
-                active:'false'
+                active:true
               },
               {
                 url: 'http://www.axshare.cn/gsc/7ANRJJ/0c/d0/8a/0cd08ad2866f481fa4e58bb5e69ca4b0/images/视频中心/u8364.png?token=c5eec950c5b983483faecff6099b68bbff19ff3fa4780f0cdadafdc5668e81c4',
                 name:'英雄联盟',
-                active:'false'
+                active:false
               },
               {
                 url: 'http://www.axshare.cn/gsc/7ANRJJ/0c/d0/8a/0cd08ad2866f481fa4e58bb5e69ca4b0/images/视频中心/u8364.png?token=c5eec950c5b983483faecff6099b68bbff19ff3fa4780f0cdadafdc5668e81c4',
                 name:'风暴英雄',
-                active:'false'
+                active:false
               },
               {
                 url: 'http://www.axshare.cn/gsc/7ANRJJ/0c/d0/8a/0cd08ad2866f481fa4e58bb5e69ca4b0/images/视频中心/u8372.png?token=01f1b64152532c01d15cb55c558d9d99e055ac71242794bc7093d0633ac8a72d',
                 name:'炉石传说',
-                active:'false'
+                active:false
               },
               {
                 url: 'http://www.axshare.cn/gsc/7ANRJJ/0c/d0/8a/0cd08ad2866f481fa4e58bb5e69ca4b0/images/视频中心/u8376.png?token=ccca0f93d6e36f55a433b6c98cb1db30f30c4b190ee518bb1c7e4d83b86a576d',
                 name:'DOTA',
-                active:'false'
+                active:false
               },
               {
                 url: 'http://www.axshare.cn/gsc/7ANRJJ/0c/d0/8a/0cd08ad2866f481fa4e58bb5e69ca4b0/images/视频中心/u8380.png?token=c1a39b51b790d00b1117a65bca1fcaffa203f34858e69cd2b89af46eaad93147',
                 name:'我的世界',
-                active:'false'
+                active:false
               },
               {
                 url: 'http://www.axshare.cn/gsc/7ANRJJ/0c/d0/8a/0cd08ad2866f481fa4e58bb5e69ca4b0/images/视频中心/u8364.png?token=c5eec950c5b983483faecff6099b68bbff19ff3fa4780f0cdadafdc5668e81c4',
                 name:'星际争霸',
-                active:'false'
+                active:false
               },
               {
                 url: 'http://www.axshare.cn/gsc/7ANRJJ/0c/d0/8a/0cd08ad2866f481fa4e58bb5e69ca4b0/images/视频中心/u8364.png?token=c5eec950c5b983483faecff6099b68bbff19ff3fa4780f0cdadafdc5668e81c4',
                 name:'体育',
-                active:'false'
+                active:false
               },
               {
                 url: 'http://www.axshare.cn/gsc/7ANRJJ/0c/d0/8a/0cd08ad2866f481fa4e58bb5e69ca4b0/images/视频中心/u8364.png?token=c5eec950c5b983483faecff6099b68bbff19ff3fa4780f0cdadafdc5668e81c4',
                 name:'动漫',
-                active:'false'
+                active:false
               },
               {
                 url: 'http://www.axshare.cn/gsc/7ANRJJ/0c/d0/8a/0cd08ad2866f481fa4e58bb5e69ca4b0/images/视频中心/u8396.png?token=71be1122fa0c4f6decfffd469fecd0c2a16dca3823f820a88534be789259d851',
                 name:'网络游戏',
-                active:'false'
+                active:false
               },
               {
                 url: 'http://www.axshare.cn/gsc/7ANRJJ/0c/d0/8a/0cd08ad2866f481fa4e58bb5e69ca4b0/images/视频中心/u8364.png?token=c5eec950c5b983483faecff6099b68bbff19ff3fa4780f0cdadafdc5668e81c4',
                 name:'星际争霸',
-                active:'false'
+                active:false
               },
               {
                 url: 'http://www.axshare.cn/gsc/7ANRJJ/0c/d0/8a/0cd08ad2866f481fa4e58bb5e69ca4b0/images/视频中心/u8372.png?token=01f1b64152532c01d15cb55c558d9d99e055ac71242794bc7093d0633ac8a72d',
                 name:'炉石传说',
-                active:'false'
+                active:false
               },
               {
                 url: 'http://www.axshare.cn/gsc/7ANRJJ/0c/d0/8a/0cd08ad2866f481fa4e58bb5e69ca4b0/images/视频中心/u8368.png?token=0f47826359ef90fbda290753dfaa3df798b9fcedf6949c7913f1deeb94c429b9',
                 name:'风暴英雄',
-                active:'false'
+                active:false
               },
               {
                 url: 'http://www.axshare.cn/gsc/7ANRJJ/0c/d0/8a/0cd08ad2866f481fa4e58bb5e69ca4b0/images/视频中心/u8364.png?token=c5eec950c5b983483faecff6099b68bbff19ff3fa4780f0cdadafdc5668e81c4',
                 name:'体育',
-                active:'false'
+                active:false
               },
               {
                 url: 'http://www.axshare.cn/gsc/7ANRJJ/0c/d0/8a/0cd08ad2866f481fa4e58bb5e69ca4b0/images/视频中心/u8364.png?token=c5eec950c5b983483faecff6099b68bbff19ff3fa4780f0cdadafdc5668e81c4',
                 name:'动漫',
-                active:'false'
+                active:false
               },
               {
                 url: 'http://www.axshare.cn/gsc/7ANRJJ/0c/d0/8a/0cd08ad2866f481fa4e58bb5e69ca4b0/images/视频中心/u8396.png?token=71be1122fa0c4f6decfffd469fecd0c2a16dca3823f820a88534be789259d851',
                 name:'网络游戏',
-                active:'false'
+                active:false
               },
               {
                 url: 'http://www.axshare.cn/gsc/7ANRJJ/0c/d0/8a/0cd08ad2866f481fa4e58bb5e69ca4b0/images/视频中心/u8364.png?token=c5eec950c5b983483faecff6099b68bbff19ff3fa4780f0cdadafdc5668e81c4',
                 name:'星际争霸',
-                active:'false'
+                active:false
               },
               {
                 url: 'http://www.axshare.cn/gsc/7ANRJJ/0c/d0/8a/0cd08ad2866f481fa4e58bb5e69ca4b0/images/视频中心/u8372.png?token=01f1b64152532c01d15cb55c558d9d99e055ac71242794bc7093d0633ac8a72d',
                 name:'炉石传说',
-                active:'false'
+                active:false
               },
               {
                 url: 'http://www.axshare.cn/gsc/7ANRJJ/0c/d0/8a/0cd08ad2866f481fa4e58bb5e69ca4b0/images/视频中心/u8368.png?token=0f47826359ef90fbda290753dfaa3df798b9fcedf6949c7913f1deeb94c429b9',
                 name:'风暴英雄',
-                active:'false'
+                active:false
               }
 
+          ],
+          playData:[
+            {
+              id:0,
+              url:'../../assets/images/video/u1692.jpg',
+              desc:'【09DOTA2单排】但是巨魔不可能死',
+              belong:'dota第一视角直播录制'
+            },
+            {
+              id:1,
+              url:'../../assets/images/video/u1696.jpg',
+              desc:'疯狂收割的血魔和辅助船长',
+              belong:'老党Haibare'
+            },
+            {
+              id:2,
+              url:'../../assets/images/video/u1698.jpg',
+              desc:'【09DOTA2单排】但是巨魔不可能死',
+              belong:'DOTA2大神学院'
+            },
+            {
+              id:3,
+              url:'../../assets/images/video/u1705.jpg',
+              desc:'疯狂收割的血魔和辅助船长',
+              belong:'2009'
+            },
+            {
+              id:4,
+              url:'../../assets/images/video/u6536.jpg',
+              desc:'黑胡子大神第一视角 发条地精',
+              belong:'DOTA2大神学院'
+            }
           ]
       }
 
--- src/views/videoCenter/Index.vue
+++ src/views/videoCenter/Index.vue
@@ -3,21 +3,32 @@
     <MapSearch Title="视频中心"/>
     <div class="vid-container">
       <div class="crumbs">
-        <div class="control-left" @click="goStep(1)"></div>
+        <div class="control-left" :style="{background:clb}" @click="goStep(1)"></div>
         <div class="video-title-list">
           <ul :style="{width:ulWidth,left:ulLeft}">
-            <li v-for="(item,index) in this.crumbsData" :key="index" :active="active">
+            <li v-for="(item,index) in this.crumbsData" @click="routerTo(item,index)" :class="{active:item.active}" :key="index">
               <img :src="item.url" alt="">
               <p>{{item.name}}</p>
             </li>
           </ul>
         </div>
-        <div class="control-right" @click="goStep(-1)"></div>
+        <div class="control-right" :style="{background:crb}" @click="goStep(-1)"></div>
       </div>
       <div class="vid-play-content">
         <div class="play-left">
-          
-          <span></span>
+          <div class="img-big">
+            <ul :style="{top:ulTop}">
+              <li v-for="(item,index) in playData" @mouseleave="continueInterval()" @mouseover="stopInterval()" :style="{backgroundImage:'url('+imgList[index]+')'}" :key="index" >
+                <span class="img-info">{{item.desc}}</span>
+              </li>
+            </ul>
+          </div>
+
+          <ul class="img-small">
+            <li v-for="(item,index) in imgList" :key="index" @click="dropTo(index)" :class="{active:index==nowIndex}">
+              <span :style="{backgroundImage:'url('+item+')'}"></span>  
+            </li>       
+          </ul>
         </div>
         <div class="play-right">
           <ul>
@@ -36,38 +47,97 @@
 
 <script>
 import { Video } from '@/services/public.js';
+import { setInterval, clearInterval } from 'timers';
 
 export default {
   data() {
     return {
       crumbsData:'',
-      ulLeft:'0'
+      ulLeft:'0',
+      playData:'',
+      timer1:'',
+      imgList:[
+        require('../../assets/images/video/u1692.jpg'),
+        require('../../assets/images/video/u1696.jpg'),
+        require('../../assets/images/video/u1698.jpg'),
+        require('../../assets/images/video/u1705.jpg'),
+        require('../../assets/images/video/u6536.jpg')
+      ],
+      nowIndex:0
     }
   },
   computed: {
     ulWidth(){
       return this.crumbsData.length*100+'px';
+    },
+    maxUlLeft(){
+      return 1000-this.crumbsData.length*100;
+    },
+    ulTop(){
+      return -this.nowIndex*385+"px";
+    },
+    clb(){
+      return parseInt(this.ulLeft)==0?'rgb(100, 100, 100)':'#303131'
+    },
+    crb(){
+      return parseInt(this.ulLeft)==1000-this.crumbsData.length*100?'rgb(100, 100, 100)':'#303131'
     }
   },
   created() {
     Video().then((res)=>{
       if(res.code==200){
         this.crumbsData=res.list;
+        this.playData=res.playData;
+        console.log(this.playData)
       }else{
         console.log(res.message)
       }
-    })
+    });
+  },
+  mounted(){
+    const _this=this;
+    this.timer1=setInterval(this.carousel,3000)
   },
   methods: {
+    carousel(){
+       if(this.nowIndex<=3){
+        this.nowIndex++
+      }else if(this.nowIndex>3){
+        
+        this.nowIndex=0;
+      }
+    },
+    dropTo(i){
+      clearInterval(this.timer1);
+      this.nowIndex=i;
+      this.timer1=setInterval(this.carousel,3000);
+    },
+    stopInterval(){
+      clearInterval(this.timer1)
+    },
+    continueInterval(){
+      this.timer1=setInterval(this.carousel,3000);
+    },
     goStep(dirc){
       if(dirc==1&&parseInt(this.ulLeft)<0){
         this.ulLeft=parseInt(this.ulLeft)+100+"px";
-      }else if(dirc==-1&&parseInt(this.ulLeft)>1000-this.crumbsData.length*100){
+      }else if(dirc==-1&&parseInt(this.ulLeft)>this.maxUlLeft){
         this.ulLeft=parseInt(this.ulLeft)-100+"px";
-      }
-      
+      } 
+    },
+    routerTo(item,index){
+      this.crumbsData.forEach(i => {
+        i.active=false;
+      });
+      item.active=true;
+      console.log('跳转至'+item.name)
     }
   },
+  beforeDestroy(){
+    if(this.timer1){
+      clearInterval(this.timer1);
+    }
+  }
 }
 </script>
 
